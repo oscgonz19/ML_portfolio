ae391809b9872e4c7160cc8274987f592d84d4e1
ozz_2 <osacrgo1917@gmail.com>
2024-08-15 12:25:00 -0500

    new portfolio section, data engineer and new projects to show

--- src/app/componets/PortfolioComponent.jsx
+++ src/app/componets/PortfolioComponent.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const projects = [
-  {
+    {
     title: 'Automated Financial Analysis using LLM and RAG Techniques',
     description: ' This project leverages advanced techniques in Language Learning Models (LLM) and Retrieval-Augmented Generation (RAG) to streamline and enhance the analysis of financial reports, specifically 10-K and 10-Q filings.',
     imageUrl: '/financialAnalisis.jpg',
@@ -18,17 +18,18 @@ const projects = [
     
   },
   {
-    title: 'Monitoring the Financial Market of Psychedelics and Cannabis Using AI',
-    description: 'This project uses Langchain to monitor the financial market of psychedelics and cannabis using AI. It leverages OpenAI API and NewsAPI to analyze financial news and provide insights.',
-    imageUrl: '/cannabisnews.jpg',
-    link: 'https://github.com/oscgonz19/PsyCannaFinance',
+    title: "Analysis of Machine Learning Models for Breast Cancer Classification",
+    description: 'This project compares the performance of supervised and unsupervised machine learning techniques for breast cancer detection. It uses the Wisconsin Breast Cancer dataset to train and evaluate the models.',
+    imageUrl: '/pexels-cottonbro.jpg',
+    link: 'https://github.com/oscgonz19/BreastCancerDetection/blob/main/AprendizajeSupervisado_NoSupervisado.ipynb',
     highlights: [
-      "Langchain for financial news analysis using OpenAI api and NewsAPI",
-      "Exploratory data analysis and visualization for insights",
+      'A project that compares supervised and unsupervised machine learning techniques.',
+      'Uses the Wisconsin Breast Cancer dataset to train and evaluate the models.'
     ],
-    tags: ['LangChain', 'OpenAI', 'Financial News'],
-    category: 'Data Science'
+    tags: ['Machine Learning', 'Breast Cancer Detection', 'Supervised Learning', 'Unsupervised Learning'],
+    category: 'Machine Learning'
   },
+
   {
     title: 'COMPAS: Exploring Bias in Criminal Profiling Management',
     description: 'Analyzed bias in criminal profiling management using the COMPAS dataset. Explored data preprocessing techniques to handle missing data and developed visualizations to illustrate the impact of bias.',
@@ -43,7 +44,19 @@ const projects = [
     category: 'Data Science'
   },
   {
-    title: 'ML Portfolio',
+    title: 'Monitoring the Financial Market of Psychedelics and Cannabis Using AI',
+    description: 'This project uses Langchain to monitor the financial market of psychedelics and cannabis using AI. It leverages OpenAI API and NewsAPI to analyze financial news and provide insights.',
+    imageUrl: '/cannabisnews.jpg',
+    link: 'https://github.com/oscgonz19/PsyCannaFinance',
+    highlights: [
+      "Langchain for financial news analysis using OpenAI api and NewsAPI",
+      "Exploratory data analysis and visualization for insights",
+    ],
+    tags: ['LangChain', 'OpenAI', 'Financial News'],
+    category: 'Data Science'
+  },
+  {
+    title: 'ML Portfolio built with Next.js and Tailwind CSS',
     description: 'A collection of data science projects that I have worked on over the years.',
     imageUrl: '/wallpaperwebMLgithub.jpg',
     link: 'https://github.com/oscgonz19/ML_portfolio',
@@ -78,6 +91,8 @@ const categoryBackgrounds = {
   "Machine Learning": "bg-cyan-950",
   "Data Science": "bg-green-800",
   "Software Developer": "bg-red-900",
+  "DataViz": "bg-purple-800",
+  "Data Engineer": "bg-teal-700",
 };
 
 export default function PortfolioComponent() {
@@ -87,7 +102,7 @@ export default function PortfolioComponent() {
     return projects
       .filter(project => project.category === selectedCategory)
       .map((project, index) => (
-        <div key={index} className="bg-gray-300 p-6 rounded-lg shadow-lg text-black flex flex-col">
+        <div key={index} className="bg-gray-300 p-6 rounded-lg shadow-lg text-black flex flex-col h-full">
           <div className="relative w-full h-48 mb-4">
             <Image
               src={project.imageUrl}
@@ -128,22 +143,28 @@ export default function PortfolioComponent() {
     <section className={`${categoryBackgrounds[selectedCategory]} text-gray-800 py-10 transition-colors duration-500`}>
       <div className="container mx-auto p-5">
         <h1 className="text-4xl font-semibold font-sans mb-4 text-center text-white">Explore Projects</h1>
-        <div className="flex justify-center items-center mb-10">
+        <div className="flex justify-center items-center mb-10 flex-wrap">
           <button
             onClick={() => setSelectedCategory("Machine Learning")}
-            className={`px-4 py-2 mx-2 rounded-lg font-semibold ${selectedCategory === "Machine Learning" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
+            className={`px-4 py-2 mx-2 my-1 rounded-lg font-semibold ${selectedCategory === "Machine Learning" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
           >
             Machine Learning
           </button>
           <button
             onClick={() => setSelectedCategory("Data Science")}
-            className={`px-4 py-2 mx-2 rounded-lg font-semibold ${selectedCategory === "Data Science" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
+            className={`px-4 py-2 mx-2 my-1 rounded-lg font-semibold ${selectedCategory === "Data Science" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
           >
             Data Science
           </button>
           <button
+            onClick={() => setSelectedCategory("Data Engineer")}
+            className={`px-4 py-2 mx-2 my-1 rounded-lg font-semibold ${selectedCategory === "Data Engineer" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
+          >
+            Data Engineer
+          </button>
+            <button
             onClick={() => setSelectedCategory("Software Developer")}
-            className={`px-4 py-2 mx-2 rounded-lg font-semibold ${selectedCategory === "Software Developer" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
+            className={`px-4 py-2 mx-2 my-1 rounded-lg font-semibold ${selectedCategory === "Software Developer" ? "bg-blue-500 text-white" : "bg-white text-blue-500"}`}
           >
             Software Development
           </button>
